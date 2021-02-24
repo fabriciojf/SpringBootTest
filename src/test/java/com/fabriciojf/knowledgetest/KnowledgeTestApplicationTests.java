804a4381cec08780f570eafde179e1dbde96ff47
fabricio <fabriciojf@gmail.com>
2021-02-24 12:52:22 -0300

    Criação do projeto com Spring Initializr

---
+++ src/test/java/com/fabriciojf/knowledgetest/KnowledgeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabriciojf.knowledgetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
